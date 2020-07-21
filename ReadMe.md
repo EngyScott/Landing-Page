c7d5367a4938439deb5b8669ebfcd211ba36efde
EngyScott <engy.scott@ymail.com>
2020-07-21 23:41:18 +0200

    navbar active, section active and up button done

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,11 @@
 # Landing-Page
 Landing page with below features: 
-- dynamic navbar 
+- Responsive web-page
+- Dynamic navbar
+- Navbar special motion effects
+- To top button with effects
+- Active section special effect
+- Active navbar link heighlight
 
 
 Languagges:
--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
-// DECLARE VARIABLES
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**************** DECLARE VARIABLES ******************/
 const sections = document.querySelectorAll('section');
 const navUL = document.querySelector('#navbar__list');
-const navBar = document.getElementsByClassName('page__header');
-
-const addActive = function(){
-    for(let section of sections){
-        const secTop = section.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
-        if(secTop >= 0 && secTop <= windowHeight){
-            if(!section.classList.contains('your-active-class')){
-                section.classList.add('your-active-class');
-            }
-        } else{
-            if(section.classList.contains('your-active-class')){
-                section.classList.remove('your-active-class')
-            }
-        }
-    }
-}
-
-
-
+const navBar = document.getElementsByClassName('navbar__menu');
+const pageHeader = document.querySelector('header.page__header');
+/**
+ * End Global Variables
+ * START FUNCTIONS
+*/
 /*********** NAVBAR SECTION ************************/
-
-/**** CREAT THE NAVBAR  ******/
+/*** CREAT THE NAVBAR ***/
 const buildNavbar = function(section, a){
     // LOOP OVER THE SECTIONS
     for(let section of sections){
@@ -41,31 +41,55 @@ const buildNavbar = function(section, a){
         li.appendChild(a);
         // APPEND LIs TO THE UL
         navUL.appendChild(li);
-        // if(section.scrollIntoView(true)){
-        //     section.classList.add('your-active-class');
-        // }else{
-        //     section.classList.remove('your-active-class');
-        // }
-        // a.addEventListener('click', linkActive(event));
-        
+    }
+}
+/*** END OF NAVBAR CREATION***/
+/*** NAVBAR EFFECTS***/
+/* Hide when not active window */
+const hideNavbar = function(){
+    const intervalId = setInterval(() => {
+        if(!window.onscroll()){
+            pageHeader.style.display = 'none';
+        }
+    }, 10000);
+}
+
+/* Active tab when sectionon screen */
+const tabActive = function(){
+    const links = document.querySelectorAll('li > a');
+    for(let a of links){
+        if(window.location.hash == a.hash){
+            a.classList.add('active')
+        }else{
+            a.classList.remove('active')
+        }
     }
 }
 
-buildNavbar()
-    
-// let scrolling = false;
-// window.scroll = () => {
-//     scrolling = true;
-//     if (scrolling) {
-//         navBar.style.display = 'block'
-//     }else{
-//         setTimeout(() => {
-//             navBar.style.display = 'none'
-//         },50);
-//     }
-// };
-
-    /**************    SCROLL TO TOP BUTTON & NAVBAR HIDDEN  ****************/
+/*** END OF NAVBAR EFFECTS***/
+// 
+// 
+/************************ SECTIONS EFFECTS **************/
+// ADD ACTIVE CLASS TO ACTIVE SECTIONS
+const sectionActive = function(){
+    for(let section of sections){
+        const secTop = section.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        if(secTop >= 0 && secTop <= windowHeight){
+            const location = window.location.toString().split('#')[0];
+            history.replaceState(null, '', location + `#${section.id}`);
+            if(!section.classList.contains('your-active-class')){
+                section.classList.add('your-active-class');
+            }
+        } else{
+            if(section.classList.contains('your-active-class')){
+                section.classList.remove('your-active-class')
+            }
+        }
+        
+    }
+}
+/**************    SCROLL TO TOP BUTTON & NAVBAR HIDDEN  ****************/
 //Define the button
 const scrlBtn = document.getElementById("scrlBtn");
 const showBtn = function(){
@@ -74,72 +98,42 @@ const showBtn = function(){
         scrlBtn.style.display = "block"
     // Otherwise hide it
     }else{
-    scrlBtn.style.display = "none"
+        scrlBtn.style.display = "none"
     }
 }
-// Listen for scroll event
-window.onscroll = function(){
-    showBtn();
-    addActive()
-}
+// 
 //Listen to a button click event and move up
 scrlBtn.addEventListener('click', function(){
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 })
-
-
-
-
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
+// 
 /**
  * End Helper Functions
- * Begin Main Functions
+ * Begin Events
  * 
 */
-
+// 
+window.onload = function(){
 // build the nav
-
-
+    buildNavbar();
+    
+}
+// Listen for scroll event
+window.onscroll = function(){
+    showBtn();
 // Add class 'active' to section when near top of viewport
+    sectionActive();
+    tabActive();
+    // hideNavbar();
+}
+
 
 
 // Scroll to anchor ID using scrollTO event
 
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+
 
 // Build menu 
 
